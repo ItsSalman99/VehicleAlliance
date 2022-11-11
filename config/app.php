c765c647c7c8b432b124992454aaa8d5b4984bc1
Salman Abbas (سلیمان) <55852619+ItsSalman99@users.noreply.github.com>
2022-11-11 10:42:33 -0800

    jwt auth added

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
